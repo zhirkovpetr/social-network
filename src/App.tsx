ffb69bc42eaed23bea63da08e4c3602b46db48ef
Petr <zhirkovpetr95@gmail.com>
2023-09-05 17:10:39 +0300

    feat: create dialogs component

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 import {Header} from "./components/header/Header";
 import {Navbar} from "./components/navbar/Navbar";
-import {Profile} from "./components/profile/Profile";
+//import {Profile} from "./components/profile/Profile";
+import {Dialogs} from "./components/dialogs/Dialogs";
 
 import s from './App.module.css';
+
 
 export const App = () => {
   return (
     <div className={s.appWrapper}>
       <Header/>
       <Navbar/>
-      <Profile/>
+      {/*<div className={s.appWrapperContent}><Profile /></div>*/}
+      <div className={s.appWrapperContent}><Dialogs /></div>
     </div>
   );
 }
